3ff5e2981144566425b4e75e6c69e657e5ca7d93
JackTubby <jack200034@hotmail.com>
2022-11-07 21:42:04 +0000

    Add imports & elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { ColourModeContext, colourModeContext, useMode } from './theme';
+// CssBaseline will reset css to defaults and ThemeProvider gives us the ability to pass the themes into material UI
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import Topbar from './scenes/global/Topbar';
+
 function App() {
+
+  const [theme, colourMode] = useMode();
+
   return (
-    <div className="App">
-    </div>
+    <ColourModeContext.Provider value={colourMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="App">
+          <main className='content'>
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColourModeContext.Provider>
   );
 }
 
